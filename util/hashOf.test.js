e64fda305511caaa4e1089d7d9af6d3787e85b6a
JaumeTorrelles <148116414+JaumeTorrelles@users.noreply.github.com>
2025-09-20 22:13:12 +0200

    feat: create and test wallet class

--- util/hashOf.test.js
+++ util/hashOf.test.js
@@ -1,9 +1,6 @@
-const Block = require('../blockchain/block');
-const { GENESIS_DATA } = require('../config');
 const hashOf = require('./hashOf')
 
 describe('hashOf()', () => {
-
 
     it('generates a sha256 hash of the input', () => {
         expect(hashOf('test')).toEqual('9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08');
